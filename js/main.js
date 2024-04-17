ab0889eaaa18319d59f1aaeec51146dba0738509
addison atkin <addisonathome@gmail.com>
2024-04-17 14:30:08 -0500

    added leaflet map background

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-//insert code here!
+//Leaflet map
+
+//intialization
+document.addEventListener('DOMContentLoaded', function() {
+    var map = L.map('map', {
+        center: [44.5, -92], // Center coordinates for Wisconsin
+        zoom: 7, 
+        zoomControl: true
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+    }).addTo(map);
+});
