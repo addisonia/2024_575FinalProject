b2f6f44f93d4360556c5cf6f032fcfe2d2500cbc
addison atkin <addisonathome@gmail.com>
2024-04-23 15:59:28 -0500

    added basemap selection swapping and fixed bug with stats dropdown menu

--- js/main.js
+++ js/main.js
@@ -3,22 +3,49 @@
 //intialization
 document.addEventListener('DOMContentLoaded', function() {
     var map = L.map('map', {
-        center: [44.5, -92], // Center coordinates for Wisconsin
-        zoom: 7, 
+        center: [44.5, -92],
+        zoom: 7,
         zoomControl: true
     });
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    var whiteBasemap = L.tileLayer('', {
+        maxZoom: 19,
+        attribution: 'White Basemap'
+    });
+
+    var openStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(map); // Start with the openStreetMap
 
+    var satelliteBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        maxZoom: 19,
+        attribution: 'Satellite Basemap'
+    });
+
+    var basemaps = {
+        "white": whiteBasemap,
+        "openstreetmap": openStreetMap,
+        "satellite": satelliteBasemap
+    };
+
+    // Initially set the current basemap to white
+    var currentBasemap = openStreetMap;
+
+    var basemapRadios = document.querySelectorAll('input[name="basemap"]');
+    basemapRadios.forEach(function(radio) {
+        radio.addEventListener('change', function() {
+            if (currentBasemap) {
+                map.removeLayer(currentBasemap);
+            }
+            currentBasemap = basemaps[this.value];
+            currentBasemap.addTo(map);
+        });
+    });
 
     // Dropdown functionality
     var chevron = document.querySelector('.stats-dropdown-chevron');
     chevron.addEventListener('click', function() {
-      this.classList.toggle('expanded');
+        this.classList.toggle('expanded');
     });
-
-
 });
