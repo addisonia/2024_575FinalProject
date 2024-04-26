4ca9658ac44a0cb9b6a37adcef7a82d9f3039922
ejburn01 <ejburn01@gmail.com>
2024-04-26 15:31:36 -0500

    power plant symbolization and transmission layer

--- js/main.js
+++ js/main.js
@@ -34,8 +34,8 @@ function createMap() {
     var currentBasemap = openStreetMap;
 
     var basemapRadios = document.querySelectorAll('input[name="basemap"]');
-    basemapRadios.forEach(function(radio) {
-        radio.addEventListener('change', function() {
+    basemapRadios.forEach(function (radio) {
+        radio.addEventListener('change', function () {
             if (currentBasemap) {
                 map.removeLayer(currentBasemap);
             }
@@ -46,33 +46,57 @@ function createMap() {
 
     // Dropdown functionality
     var chevron = document.querySelector('.stats-dropdown-chevron');
-    chevron.addEventListener('click', function() {
+    chevron.addEventListener('click', function () {
         this.classList.toggle('expanded');
     });
     getData();
 };
 
-function pointToLayer(feature, latlng, attributes){
+function calcColor(attValue) {
+    var color
+    if (attValue <= 5) {
+        color = "#ffeda0"
+    }
+    else if (attValue <= 25) {
+        color = "#fec44f"
+    }
+    else {
+        color = "#f03b20"
+    }
+
+    linearScale = d3.scaleLinear()
+        .domain([0, 20])
+        .range(["#ffeda0", "#f03b20"]);
+
+    return linearScale(attValue);
+};
+
+function lineToLayer() {
+
+}
+
+function pointToLayer(feature, latlng, attributes) {
 
     //console.log(attributes[0])
     var valueDict = {};
 
-    attributes.forEach(function(attribute) {
+    attributes.forEach(function (attribute) {
         var attValue = feature.properties[attribute];
         valueDict[attribute] = attValue
 
     });
     //console.log(valueDict)
-    
+
     //create marker options
     var options = {
-        fillColor: "#ff7800",
         color: "#000",
         weight: 1,
         opacity: 1,
-        radius:7,
+        radius: 7,
         fillOpacity: 0.65
     };
+
+    options.fillColor = calcColor(valueDict["NEAR_DIST"]);
 
     //create circle marker layer
     var layer = L.circleMarker(latlng, options);
@@ -80,28 +104,28 @@ function pointToLayer(feature, latlng, attributes){
     //build popup content string
     var popupContent = new PopupContent(feature.properties, valueDict);
 
-    layer.bindPopup(popupContent.formatted,{
-        offset: new L.Point(0,-Math.sqrt(options.radius))
+    layer.bindPopup(popupContent.formatted, {
+        offset: new L.Point(0, -Math.sqrt(options.radius))
     });
 
     //return the circle marker to the L.geoJson pointToLayer option
     return layer;
 };
 
-function createPropSymbols(data, attributes){
+function createPropSymbols(data, attributes) {
     //console.log(data)
     L.geoJson(data, {
-        pointToLayer: function(feature, latlng){
+        pointToLayer: function (feature, latlng) {
             return pointToLayer(feature, latlng, attributes);
         }
     }).addTo(map);
 };
 
-function PopupContent(properties, valueDict){
+function PopupContent(properties, valueDict) {
     this.formatted = "<p><b>Plant Name:</b> " + valueDict["Plant_Name"] + "</p><p><b>Primary Source:</b> " + valueDict["PrimSource"] + "</p><p><b>Distance to high-voltage transmission:</b> " + valueDict["NEAR_DIST"] + " miles</p>";
 };
 
-function processData(data){
+function processData(data) {
     //empty array to hold attributes
     var attributes = [];
 
@@ -110,9 +134,9 @@ function processData(data){
     console.log(properties)
 
     //push each attribute name into attributes array
-    for (var attribute in properties){
+    for (var attribute in properties) {
         //only take attributes with population values
-        if (attribute == "Plant_Name" || attribute == "NEAR_DIST" || attribute == "PrimSource"){
+        if (attribute == "Plant_Name" || attribute == "NEAR_DIST" || attribute == "PrimSource") {
             attributes.push(attribute);
         };
     };
@@ -120,18 +144,25 @@ function processData(data){
     return attributes;
 };
 
-function getData(){
+function getData() {
     //load the data
     fetch("data/PowerPlants_Continental_US_project.geojson")
-        .then(function(response){
+        .then(function (response) {
             return response.json();
         })
-        .then(function(json){
+        .then(function (json) {
             var attributes = processData(json);
             createPropSymbols(json, attributes);
         })
+    fetch("data/Transmission_Continental_US.geojson")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            L.geoJson(data).addTo(map);
+        })
 };
 
-document.addEventListener('DOMContentLoaded',createMap())
+document.addEventListener('DOMContentLoaded', createMap())
 
 
